3e087b94da734c2edaaeda3829d3323ffd217997
Irsyan12 <irsyah@mhs.usk.ac.id>
2023-11-18 14:15:34 +0700

    .

--- lib/detail_screen.dart
+++ lib/detail_screen.dart
@@ -1,4 +1,7 @@
+import 'package:chewie/chewie.dart';
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class DetailScreen extends StatelessWidget {
   final agent; // Sesuaikan tipe data dengan model Agent yang Anda miliki
@@ -9,26 +12,203 @@ class DetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Detail Screen'),
+        backgroundColor: Colors.black,
+        title: Text(
+          '${agent.name}',
+          style: const TextStyle(
+            fontFamily: 'valorant',
+            fontSize: 20.0, // Ukuran font
+            fontWeight: FontWeight.bold, // Ketebalan font
+            color: Colors.red, // Warna font
+          ),
+        ),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
+      body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              'Name: ${agent.name}', // Sesuaikan dengan properti yang dimiliki oleh model Agent
-              style: TextStyle(fontSize: 20),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Stack(
+              children: <Widget>[Image.asset(agent.imageAsset2)],
             ),
-            SizedBox(height: 16),
-            Text(
-              'Role: ${agent.role}', // Sesuaikan dengan properti yang dimiliki oleh model Agent
-              style: TextStyle(fontSize: 18),
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const Text(
+                    '//Peran',
+                    style:
+                        TextStyle(fontSize: 11, fontWeight: FontWeight.normal),
+                  ),
+                  Text(
+                    '${agent.role}',
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                  const SizedBox(height: 17),
+                  const Text('//Biografi',
+                      style: TextStyle(
+                        fontSize: 12,
+                        fontWeight: FontWeight.w400,
+                      )),
+                  Text(
+                    '${agent.description}',
+                    style: const TextStyle(
+                      color: Colors.black,
+                      fontSize: 11,
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  DefaultTabController(
+                    length: 4,
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.stretch,
+                      children: [
+                        TabBar(
+                          labelColor: Colors.black,
+                          tabs: [
+                            Tab(
+                              child: FittedBox(
+                                fit: BoxFit.scaleDown,
+                                alignment: Alignment.center,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SvgPicture.asset(
+                                      'svgs/${agent.cskill.toLowerCase().replaceAll(' ', '-')}.svg',
+                                      height: 50,
+                                    ),
+                                    const SizedBox(height: 20),
+                                    Text('C', style: TextStyle(fontSize: 16)),
+                                    Text('${agent.cskill}',
+                                        overflow: TextOverflow.ellipsis),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: FittedBox(
+                                fit: BoxFit.scaleDown,
+                                alignment: Alignment.center,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SvgPicture.asset(
+                                      'svgs/${agent.qskill.toLowerCase().replaceAll(' ', '-')}.svg',
+                                      height: 50,
+                                    ),
+                                    const SizedBox(height: 20),
+                                    Text('Q', style: TextStyle(fontSize: 16)),
+                                    Text('${agent.qskill}',
+                                        overflow: TextOverflow.ellipsis),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: FittedBox(
+                                fit: BoxFit.scaleDown,
+                                alignment: Alignment.center,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SvgPicture.asset(
+                                        'svgs/${agent.eskill.toLowerCase().replaceAll(' ', '-')}.svg',
+                                        height: 50),
+                                    const SizedBox(height: 20),
+                                    Text('E', style: TextStyle(fontSize: 16)),
+                                    Text('${agent.eskill}',
+                                        overflow: TextOverflow.ellipsis),
+                                  ],
+                                ),
+                              ),
+                            ),
+                            Tab(
+                              child: FittedBox(
+                                fit: BoxFit.scaleDown,
+                                alignment: Alignment.center,
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    SvgPicture.asset(
+                                        'svgs/${agent.xskill.toLowerCase().replaceAll(' ', '-')}.svg',
+                                        height: 50),
+                                    const SizedBox(height: 20),
+                                    Text('X', style: TextStyle(fontSize: 16)),
+                                    Text('${agent.xskill}',
+                                        overflow: TextOverflow.ellipsis),
+                                  ],
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 300,
+                          child: TabBarView(
+                            children: [
+                              ChewieVideoWidget(videoUrl: agent.cVideoUrl),
+                              ChewieVideoWidget(videoUrl: agent.qVideoUrl),
+                              ChewieVideoWidget(videoUrl: agent.eVideoUrl),
+                              ChewieVideoWidget(videoUrl: agent.xVideoUrl),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
             ),
-            // Tambahkan widget lain sesuai kebutuhan untuk menampilkan informasi tambahan
           ],
         ),
       ),
     );
+  }
+}
+
+class ChewieVideoWidget extends StatefulWidget {
+  final String videoUrl;
+
+  ChewieVideoWidget({required this.videoUrl});
+
+  @override
+  _ChewieVideoWidgetState createState() => _ChewieVideoWidgetState();
+}
+
+class _ChewieVideoWidgetState extends State<ChewieVideoWidget> {
+  late ChewieController _chewieController;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeVideoPlayer();
+  }
+
+  void _initializeVideoPlayer() {
+    _chewieController = ChewieController(
+        // ignore: deprecated_member_use
+        videoPlayerController: VideoPlayerController.network(widget.videoUrl),
+        autoPlay: true,
+        looping: true,
+        aspectRatio: 16 / 9,
+        showControls: false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 200, // Sesuaikan dengan tinggi yang diinginkan
+      child: Chewie(controller: _chewieController),
+    );
+  }
+
+  @override
+  void dispose() {
+    _chewieController.dispose();
+    super.dispose();
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -2,11 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:valorant_app/detail_screen.dart';
 import 'package:valorant_app/model/agent.dart';
 
-class AgentListScreen extends StatelessWidget {
+class AgentListScreen extends StatefulWidget {
   const AgentListScreen({Key? key}) : super(key: key);
 
   @override
+  _AgentListScreenState createState() => _AgentListScreenState();
+}
+
+class _AgentListScreenState extends State<AgentListScreen> {
+  String selectedRole = 'All'; // Default: Menampilkan semua agen
+
+  @override
   Widget build(BuildContext context) {
+    // Mengurutkan agentList berdasarkan nama agen
+    agentList.sort((a, b) => a.name.compareTo(b.name));
+
+    // Menyaring agentList berdasarkan peran yang dipilih
+    List<agent> filteredList = agentList
+        .where((agent) => selectedRole == 'All' || agent.role == selectedRole)
+        .toList();
+
     return Material(
       child: Scaffold(
         appBar: AppBar(
@@ -14,59 +29,105 @@ class AgentListScreen extends StatelessWidget {
           title: Text(
             'Valorant',
             style: TextStyle(
-              fontSize: 20.0, // Ukuran font
-              fontWeight: FontWeight.bold, // Ketebalan font
-              color: Colors.red, // Warna font
+              fontFamily: 'valorant',
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              color: Color(0xFFFD6969),
             ),
           ),
         ),
         body: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: ListView.builder(
-            itemBuilder: (context, index) {
-              final agent = agentList[index];
-              return InkWell(
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (context) {
-                    return DetailScreen(agent: agentList[index]);
-                  }));
-                },
-                child: Card(
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Expanded(
-                        flex: 1,
-                        child: Image.asset(agent.imageAsset),
-                      ),
-                      Expanded(
-                        flex: 2,
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                agent.name,
-                                style: const TextStyle(fontSize: 16.0),
-                              ),
-                              const SizedBox(
-                                height: 10,
-                              ),
-                              Text(agent.role),
-                            ],
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            children: [
+              SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    buildRoleRadioButton('All'),
+                    buildRoleRadioButton('Duelist'),
+                    buildRoleRadioButton('Initiator'),
+                    buildRoleRadioButton('Controller'),
+                    buildRoleRadioButton('Sentinel'),
+                  ],
                 ),
-              );
-            },
-            itemCount: agentList.length,
+              ),
+              const SizedBox(height: 10),
+              Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    final agent = filteredList[index];
+                    return InkWell(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) {
+                            return DetailScreen(agent: agent);
+                          }),
+                        );
+                      },
+                      child: Card(
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Expanded(
+                              flex: 1,
+                              child: Image.asset(
+                                agent.imageAsset,
+                                height:
+                                    100,
+                              ),
+                            ),
+                            Expanded(
+                              child: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Text(
+                                      agent.name,
+                                      style: const TextStyle(
+                                        fontSize: 16.0,
+                                        fontWeight: FontWeight.w500,
+                                      ),
+                                    ),
+                                    const SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(agent.role),
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    );
+                  },
+                  itemCount: filteredList.length,
+                ),
+              ),
+            ],
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildRoleRadioButton(String role) {
+    return Row(
+      children: [
+        Radio(
+          value: role,
+          groupValue: selectedRole,
+          onChanged: (value) {
+            setState(() {
+              selectedRole = value.toString();
+            });
+          },
+        ),
+        Text(role),
+      ],
     );
   }
 }
